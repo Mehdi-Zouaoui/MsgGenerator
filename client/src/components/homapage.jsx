99388fbb80264edbbd4ea98c4c9ac8c4cbcfc74d
Your Name <md1.zouaoui@gmail.com>
2020-03-30 19:08:57 +0200

    Token Acces WIP correction

--- client/src/components/homapage.jsx
+++ client/src/components/homapage.jsx
@@ -3,26 +3,26 @@ import React from "react";
 
 class HomePage extends React.Component {
 
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            token: this.props.cookies.get('token'),
-        }
-    };
-
-
-    doYouHaveCookie() {
-        if (this.state.cookie) {
-            console.log('Access Granted')
-        } else console.log('Access Denied')
-    }
+    //
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         token: this.props.cookies.get('token'),
+    //     }
+    // };
+    //
+    //
+    // doYouHaveCookie() {
+    //     if (this.state.cookie) {
+    //         console.log('Access Granted')
+    //     } else console.log('Access Denied')
+    // }
 
     render() {
         return (
             <div>
                 <h2> HomePage</h2>
-                <button onClick={this.doYouHaveCookie()}> Cookie</button>
+                {/*<button onClick={this.doYouHaveCookie()}> Cookie</button>*/}
             </div>
 
         )
